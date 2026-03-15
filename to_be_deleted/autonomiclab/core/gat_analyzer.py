d61e2ed363d34322f128c40405c238452f3e06f7
John Hansen <john.hansen.aal@gmail.com>
2026-03-15 14:31:42 +0100

    Cleanup: add utils modules, remove to_be_deleted and stray files

--- to_be_deleted/autonomiclab/core/gat_analyzer.py
+++
@@ -1,9 +0,0 @@
-"""Main GAT analysis engine"""
-
-class GATAnalyzer:
-    def __init__(self, dataset):
-        self.dataset = dataset
-    
-    def analyze(self):
-        """Run complete GAT analysis"""
-        pass
--- to_be_deleted/autonomiclab/utils/validators.py
+++
@@ -1,9 +0,0 @@
-"""Data validation"""
-
-def validate_dataset(dataset):
-    """Validate dataset integrity"""
-    if not dataset.signals:
-        return False, "No signals found"
-    if not dataset.markers:
-        return False, "No markers found"
-    return True, "Valid"
